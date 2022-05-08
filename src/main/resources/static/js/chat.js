c739cfaee39efc28a01b322a9f97ad63a5fc787d
hongseungkeun <eiziong1@gmail.com>
2022-05-09 01:18:42 +0900

    프로젝트 리스트 및 채팅 어사이드 분리

---
+++ src/main/resources/static/js/chat.js
@@ -0,0 +1,13 @@
+/* chatting */
+
+let count = 1;
+
+$("div.chattingOpen").on("click", function () {
+    $(this).removeClass("open");
+    $("div.chatting").addClass("open");
+})
+
+$("div.foldChatButton").on("click", function () {
+    $(this).parents("div.chatting").removeClass("open");
+    $("div.chattingOpen").addClass("open");
+})
--- src/main/resources/static/js/projectDetail.js
+++ src/main/resources/static/js/projectDetail.js
@@ -117,20 +117,3 @@ $("div.projectTabMenu").find("li").on("click", function() {
     }
 })
 
-
-
-
-
-/* chatting */
-
-let count = 1;
-
-$("div.chattingOpen").on("click", function () {
-    $(this).removeClass("open");
-    $("div.chatting").addClass("open");
-})
-
-$("div.foldChatButton").on("click", function () {
-    $(this).parents("div.chatting").removeClass("open");
-    $("div.chattingOpen").addClass("open");
-})
