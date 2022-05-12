e5309eafc566db29ceeab1b447b095c524f61dbb
SangAya <sanga0830@naver.com>
2022-05-12 17:14:04 +0900

    storywrite , storymodify , projectDetail modal,css,html,js 완료 및 storymodify.css+storymodify.js 생성

---
+++ src/main/resources/static/js/storymodify.js
@@ -0,0 +1,11 @@
+
+
+// 스토리 수정 확인모달
+$(".modifyMd").on("click",function(){
+     $(".modalStory1").css('display','block');  
+ 
+});
+
+$(".xBtns").on("click",function(){
+    $(".modals").hide();
+});
