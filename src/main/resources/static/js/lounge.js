ab1cce6dd8b7e2327312dee33532310495cdf99f
SangAya <sanga0830@naver.com>
2022-05-12 18:39:05 +0900

    스토리 라운지 수정

--- src/main/resources/static/js/lounge.js
+++ src/main/resources/static/js/lounge.js
@@ -11,3 +11,35 @@ $(".replyOpen").each(function (i,open) {
         }
     })
 })
+
+//프로필 상세보기(댓글의댓글)
+$(".activeProName").on("mouseover", function () {
+    $(this).next().show();
+})
+
+$(".activeProName").on("mouseout", function () {
+    $(this).next().hide();
+})
+$(".hoverUl").on("mouseover", function () {
+    $(this).show();
+})
+
+$(".hoverUl").on("mouseout", function () {
+    $(this).hide();
+})
+
+////프로필 상세보기(댓글)
+$(".proNames").on("mouseover", function () {
+    $(this).next().show();
+})
+
+$(".proNames").on("mouseout", function () {
+    $(this).next().hide();
+})
+$(".hoverUl").on("mouseover", function () {
+    $(this).show();
+})
+
+$(".hoverUl").on("mouseout", function () {
+    $(this).hide();
+})
