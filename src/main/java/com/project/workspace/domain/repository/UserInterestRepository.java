06eb41ddc3a3b023574225d59ceb7484ecf58fdb
SangAya <sanga0830@naver.com>
2022-05-17 23:45:36 +0900

    마이페이지

--- src/main/java/com/project/workspace/domain/repository/UserInterestRepository.java
+++ src/main/java/com/project/workspace/domain/repository/UserInterestRepository.java
@@ -1,7 +1,12 @@
 package com.project.workspace.domain.repository;
 
 import com.project.workspace.domain.vo.UserInterestVO;
+import com.project.workspace.domain.vo.UserTagVO;
+import com.project.workspace.domain.vo.UserVO;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserInterestRepository extends JpaRepository<UserInterestVO, Long> {
+    public List<UserInterestVO> findByUserVO(UserVO userVO);
 }
