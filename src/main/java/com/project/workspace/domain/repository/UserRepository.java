b3861b550df6ef35ca03acde2480699cfbe2e811
SongJunHyeong <thdwnsgud79@naver.com>
2022-05-24 00:28:20 +0900

    no message

--- src/main/java/com/project/workspace/domain/repository/UserRepository.java
+++ src/main/java/com/project/workspace/domain/repository/UserRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserVO, Long> {
     public UserVO findByUserId(String userId);
-    public UserVO findByUserNickname(String userNickName);
 }
